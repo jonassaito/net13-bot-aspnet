6ac70388b4034a1ebdbb64b7a16957689b2b3e19
jonassaito <jonassaitobr@gmail.com>
2018-09-06 02:42:04 -0300

    ok

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,9 +2,7 @@
 using MongoDB.Driver;
 using SimpleBot.Logic;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace SimpleBot
 {
--- Logic/UserProfile.cs
+++ Logic/UserProfile.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SimpleBot
+﻿namespace SimpleBot
 {
     public class UserProfile
     {
